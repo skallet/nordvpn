9ce20e33d80fe8cddb30c8909fb486a0d564e8bb
skall <blazekm1lan@seznam.cz>
2019-06-24 16:55:14 +0200

    Nordvpn fix

--- nordVpn.sh
+++ nordVpn.sh
@@ -82,7 +82,7 @@ country_filter() { # curl -s "https://api.nordvpn.com/v1/servers/countries" | jq
     local nordvpn_api=$1 country=(${COUNTRY//[;,]/ })
     if [[ ${#country[@]} -ge 1 ]]; then
         country=${country[0]//_/ }
-        local country_id=`curl -s "${nordvpn_api}/v1/servers/countries" | jq -r ".[] |
+        local country_id=`curl -s "${nordvpn_api}?route=/nordproxy&nord-route=/servers/countries" | jq -r ".[] |
                           select( (.name|test(\"^${country}$\";\"i\")) or
                                   (.code|test(\"^${country}$\";\"i\")) ) |
                           .id" | head -n 1`
@@ -97,7 +97,7 @@ group_filter() { # curl -s "https://api.nordvpn.com/v1/servers/groups" | jq -r '
     local nordvpn_api=$1 category=(${CATEGORY//[;,]/ })
     if [[ ${#category[@]} -ge 1 ]]; then
         category=${category[0]//_/ }
-        local identifier=`curl -s "${nordvpn_api}/v1/servers/groups" | jq -r ".[] |
+        local identifier=`curl -s "${nordvpn_api}?route=/nordproxy&nord-route=/servers/groups" | jq -r ".[] |
                           select( .title | test(\"${category}\";\"i\") ) |
                           .identifier" | head -n 1`
         if [[ -n ${identifier} ]]; then
@@ -131,11 +131,11 @@ select_hostname() {
     filters+="$(technology_filter )"
 
     lim=${LIMITS:-5}
-    echo "Fetching: ${nordvpn_api}?route=/nordproxy&nord-route=/recommendations&${filters}limit=${lim}"
-    hostname=`curl -s "${nordvpn_api}?route=/nordproxy&nord-route=/recommendations&${filters}limit=${lim}" | jq -r ".[].hostname" | shuf | head -n 1`
+    echo "Fetching: ${nordvpn_api}?route=/nordproxy&nord-route=/servers/recommendations&${filters}limit=${lim}" > /dev/stderr
+    hostname=`curl -s "${nordvpn_api}?route=/nordproxy&nord-route=/servers/recommendations&${filters}limit=${lim}" | jq -r ".[].hostname" | shuf | head -n 1`
     if [[ -z ${hostname} ]]; then
         echo "Unable to find a server with the specified parameters, using any recommended server" > /dev/stderr
-        hostname=`curl -s "${nordvpn_api}?route=/nordproxy&nord-route=/recommendations&limit=1" | jq -r ".[].hostname"`
+        hostname=`curl -s "${nordvpn_api}?route=/nordproxy&nord-route=/servers/recommendations&limit=1" | jq -r ".[].hostname"`
     fi
 
     echo "Best server : ${hostname}" > /dev/stderr
